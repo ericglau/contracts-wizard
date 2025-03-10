0a9f5da29eefc0f8481640aeb0fe1006ad178b64
Eric Lau <ericglau@outlook.com>
2025-03-10 17:20:18 -0400

    Select app based on lang and requested version

--- packages/ui/src/main.ts
+++ packages/ui/src/main.ts
@@ -3,13 +3,17 @@ import './common/styles/global.css';
 import type {} from 'svelte';
 import SolidityApp from './solidity/App.svelte';
 import CairoApp from './cairo/App.svelte';
+import CairoNextApp from './cairo-next/App.svelte';
 import StellarApp from './stellar/App.svelte';
 import StylusApp from './stylus/App.svelte';
 import { postMessage } from './common/post-message';
 import UnsupportedVersion from './common/UnsupportedVersion.svelte';
 import semver from 'semver';
-import { compatibleContractsSemver as compatibleSolidityContractsSemver } from '@openzeppelin/wizard';
-import { compatibleContractsSemver as compatibleCairoContractsSemver } from '@openzeppelin/wizard-cairo';
+import { compatibleContractsSemver as soliditySemver } from '@openzeppelin/wizard';
+import { compatibleContractsSemver as cairoSemver } from '@openzeppelin/wizard-cairo';
+import { compatibleContractsSemver as cairoNextSemver } from '@openzeppelin/wizard-cairo-next';
+import { compatibleContractsSemver as stellarSemver } from '@openzeppelin/wizard-stellar';
+import { compatibleContractsSemver as stylusSemver } from '@openzeppelin/wizard-stylus';
 import type { InitialOptions } from './common/initial-options.ts';
 
 function postResize() {
@@ -34,19 +38,81 @@ const initialOpts: InitialOptions = {
   premint: params.get('premint') ?? undefined,
 };
 
-const compatibleVersionSemver = lang === 'cairo' ? compatibleCairoContractsSemver : compatibleSolidityContractsSemver;
+const appTypes = ['solidity', 'cairo', 'cairo-next', 'stellar', 'stylus'] as const;
+type AppType = typeof appTypes[number];
+
+interface CompatibleSelection {
+  compatible: true;
+  appType: AppType;
+}
+
+interface IncompatibleSelection {
+  compatible: false;
+  compatibleVersionsSemver: string;
+}
+
+function evalutateSelection(lang: string | undefined, requestedVersion: string | undefined): CompatibleSelection | IncompatibleSelection {
+  switch (lang) {
+    case 'cairo': {
+      if (requestedVersion === undefined) {
+        return { compatible: true, appType: 'cairo' };
+      } else if (semver.satisfies(requestedVersion, cairoNextSemver)) {
+        return { compatible: true, appType: 'cairo-next' };
+      } else if (semver.satisfies(requestedVersion, cairoSemver)) {
+        return { compatible: true, appType: 'cairo' };
+      } else {
+        return { compatible: false, compatibleVersionsSemver: `${cairoNextSemver} || ${cairoSemver}` };
+      }
+    }
+    case 'stellar': {
+      if (requestedVersion === undefined || semver.satisfies(requestedVersion, stellarSemver)) {
+        return { compatible: true, appType: 'stellar' };
+      } else {
+        return { compatible: false, compatibleVersionsSemver: stellarSemver };
+      }
+    }
+    case 'stylus': {
+      if (requestedVersion === undefined || semver.satisfies(requestedVersion, stylusSemver)) {
+        return { compatible: true, appType: 'stylus' };
+      } else {
+        return { compatible: false, compatibleVersionsSemver: stylusSemver };
+      }
+    }
+    case 'solidity':
+    case undefined:
+    default: {
+      if (requestedVersion === undefined || semver.satisfies(requestedVersion, soliditySemver)) {
+        return { compatible: true, appType: 'solidity' };
+      } else {
+        return { compatible: false, compatibleVersionsSemver: soliditySemver };
+      }
+    }
+  }
+};
 
 let app;
-if (requestedVersion && !semver.satisfies(requestedVersion, compatibleVersionSemver)) {
+const selection = evalutateSelection(lang, requestedVersion);
+
+if (!selection.compatible) {
+  if (requestedVersion === undefined) { // Should never happen, since undefined should lead to a compatible selection
+    throw new Error('requestedVersion is undefined');
+  }
+
   postMessage({ kind: 'oz-wizard-unsupported-version' });
   app = new UnsupportedVersion({
     target: document.body,
-    props: { requestedVersion, compatibleVersionSemver },
+    props: { requestedVersion: requestedVersion!, compatibleVersionSemver: selection.compatibleVersionsSemver },
   });
 } else {
-  switch (lang) {
+  switch (selection.appType) {
     case 'cairo':
       app = new CairoApp({
+        target: document.body,
+        props: { initialTab, initialOpts },
+      });
+      break;
+    case 'cairo-next':
+      app = new CairoNextApp({
         target: document.body,
         props: { initialTab, initialOpts },
       });
@@ -58,12 +124,15 @@ if (requestedVersion && !semver.satisfies(requestedVersion, compatibleVersionSem
       app = new StylusApp({ target: document.body, props: { initialTab, initialOpts } });
       break;
     case 'solidity':
-    default:
       app = new SolidityApp({
         target: document.body,
         props: { initialTab, initialOpts },
       });
       break;
+    default: {
+      const _: never = selection.appType;
+      throw new Error('Unknown app type');
+    }
   }
 }
 
