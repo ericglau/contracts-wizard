210dc7f253d954e63ef818876032272695e33ee0
Eric Lau <ericglau@outlook.com>
2025-04-25 13:57:01 -0400

    WIP

--- scripts/release/format-changelog.js
+++ scripts/release/format-changelog.js
@@ -4,38 +4,27 @@
 // This is run automatically when npm version is run.
 
 const fs = require('fs');
-const { getSupportedLanguageInCoreSubfolder } = require('./language-input.mjs');
 
-function formatChangelog(dir) {
-  const changelogPath = fs.join(dir, 'CHANGELOG.md');
+const changelog = fs.readFileSync('CHANGELOG.md', 'utf8');
 
-  const changelog = fs.readFileSync(changelogPath, 'utf8');
+// Groups:
+//  - 1: Pull Request Number and URL
+//  - 2: Changeset entry
+const RELEASE_LINE_REGEX = /^- (\[#.*?\]\(.*?\))?.*?! - (.*)$/gm;
 
-  // Groups:
-  //  - 1: Pull Request Number and URL
-  //  - 2: Changeset entry
-  const RELEASE_LINE_REGEX = /^- (\[#.*?\]\(.*?\))?.*?! - (.*)$/gm;
+// Captures X.Y.Z or X.Y.Z-rc.W
+const VERSION_TITLE_REGEX = /^## (\d+\.\d+\.\d+(-rc\.\d+)?)$/gm;
 
-  // Captures X.Y.Z or X.Y.Z-rc.W
-  const VERSION_TITLE_REGEX = /^## (\d+\.\d+\.\d+(-rc\.\d+)?)$/gm;
+const formatted = changelog
+  // Remove titles
+  .replace(/^### Major Changes\n\n/gm, '')
+  .replace(/^### Minor Changes\n\n/gm, '')
+  .replace(/^### Patch Changes\n\n/gm, '')
+  // Remove extra whitespace between items
+  .replace(/^(- \[.*\n)\n(?=-)/gm, '$1')
+  // Format each release line
+  .replace(RELEASE_LINE_REGEX, (_, pr, entry) => (pr ? `- ${entry} (${pr})` : `- ${entry}`))
+  // Add date to new version
+  .replace(VERSION_TITLE_REGEX, `\n## $1 (${new Date().toISOString().split('T')[0]})`);
 
-  const formatted = changelog
-    // Remove titles
-    .replace(/^### Major Changes\n\n/gm, '')
-    .replace(/^### Minor Changes\n\n/gm, '')
-    .replace(/^### Patch Changes\n\n/gm, '')
-    // Remove extra whitespace between items
-    .replace(/^(- \[.*\n)\n(?=-)/gm, '$1')
-    // Format each release line
-    .replace(RELEASE_LINE_REGEX, (_, pr, entry) => (pr ? `- ${entry} (${pr})` : `- ${entry}`))
-    // Add date to new version
-    .replace(VERSION_TITLE_REGEX, `\n## $1 (${new Date().toISOString().split('T')[0]})`);
-
-  fs.writeFileSync(changelogPath, formatted);
-}
-
-const languageFolders = getSupportedLanguageInCoreSubfolder();
-for (const languageFolder of languageFolders) {
-  console.log(`Formatting changelog for ${languageFolder}...`);
-  formatChangelog(languageFolder);
-}
+fs.writeFileSync('CHANGELOG.md', formatted);
