ab2c2efcd7a62821943052753d04a82270fb8566
Eric Lau <ericglau@outlook.com>
2025-06-04 14:30:00 -0400

    Remove other examples

--- packages/chat-sample/src/extension.ts
+++ packages/chat-sample/src/extension.ts
@@ -1,13 +1,9 @@
 import * as vscode from 'vscode';
-import { registerChatLibChatParticipant } from './chatUtilsSample';
-import { registerSimpleParticipant } from './simple';
 import { registerToolUserChatParticipant } from './toolParticipant';
 import { registerChatTools } from './tools';
 
 export function activate(context: vscode.ExtensionContext) {
-    registerSimpleParticipant(context);
     registerToolUserChatParticipant(context);
-    registerChatLibChatParticipant(context);
 
     registerChatTools(context);
 }
